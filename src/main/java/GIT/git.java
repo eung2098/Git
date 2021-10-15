b5244223675b4a0ebad44915d2fe7dc7a77fa754
eung2098 <jeg8421@naver.com>
2021-10-15 20:14:57 +0900

    git 연습파일

---
+++ src/main/java/GIT/git.java
@@ -0,0 +1,14 @@
+package GIT;
+
+public class git {
+
+    String name;
+    String number;
+
+    public void dog(String name, String number){
+        this.name = "eung";
+        this.number = "1234";
+
+        System.out.println("개의 이름은" + name + "번호는" + number +"입니다.");
+    }
+}
---
+++ src/test/java/GIT/GitApplicationTests.java
@@ -0,0 +1,13 @@
+package GIT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
